2fca8dc1e2f890db21784eb6bae6509c0d0b14e3
shuu1412 <sasuke22062002@gmail.com>
2021-11-12 21:27:20 +0700

    KT

--- Kiểm Tra/Bai4.py
+++ Kiểm Tra/Bai4.py
@@ -1,19 +1,15 @@
 s=str(input())
-s1=""
+n=int(input())
 cnt=1
-s2=""
-for i in range(len(s)-1):
-    if s[i]!=s[i+1]:
-        s1=s1+str(cnt)+s[i]
-        cnt=1
-    else: cnt+=1
-s1=s1+str(cnt)+s[len(s)-1]
-cnt=1
-for i in range(len(s1)-1):
-    if s1[i]!=s1[i+1]:
-        s2=s2+str(cnt)+s1[i]
-        cnt=1
-    else: cnt+=1
-s2=s2+str(cnt)+s1[len(s1)-1]
-print(s1)
-print(s2)
+while(n>0):
+    s1=""
+    for i in range(len(s)-1):
+        if s[i]!=s[i+1]:
+            s1=s1+str(cnt)+s[i]
+            cnt=1
+        else: cnt+=1
+    s1=s1+str(cnt)+s[len(s)-1]
+    cnt=1
+    print(s1)
+    s=s1
+    n-=1
